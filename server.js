43bf0286a9f88e77735cbf668eb0ac7e7a938152
serviulautaro <jacc965@gmail.com>
2026-06-03 21:42:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (!fs.existsSync(docsDir)) fs.mkdirSync(docsDir);
 const db = new Database(path.join(__dirname, 'serviu.db'));
 
 const SUPABASE_URL = 'https://qirjfgjesjzikouehmib.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpcmpmZ2plc2p6aWtvdWVobWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NjgxMTUsImV4cCI6MjA5MzI0NDExNX0.7bDpXPZyc-Ovt-EWBqCl3RsbPqiU_eSAa98F_ufbVqU';
+const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpcmpmZ2plc2p6aWtvdWVobWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NjgxMTUsImV4cCI6MjA5MzI0NDExNX0.7bDpXPZyc-Ovt-EWBqCl3RsbPqiU_eSAa98F_ufbVqU';
 const supabaseServer = createClient(SUPABASE_URL, SUPABASE_KEY);
 const pgPool = process.env.DATABASE_URL
   ? new Pool({
